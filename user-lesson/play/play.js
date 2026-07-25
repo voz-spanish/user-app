091481187646d61d84a97ce64c7f14839b8e5ac9
voz <japones.con.coco@gmail.com>
2026-07-26 07:17:57 +0900

    Update play.js

--- user-lesson/play/play.js
+++ user-lesson/play/play.js
@@ -1,3 +1,6 @@
+;(function () {
+'use strict'
+
 const SUPABASE_URL = 'https://nsprbshgkxywtwmimkcy.supabase.co'
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5zcHJic2hna3h5d3R3bWlta2N5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgzMzMxMDAsImV4cCI6MjA5MzkwOTEwMH0.g51y4rq3xEDYD9GJoux7UDBeOpXyqYLDptwQ3LHy6b8'
 const { createClient } = supabase
@@ -1393,3 +1396,5 @@ document.getElementById('stop-confirm-btn').addEventListener('click', async () =
   document.getElementById('loading-msg').style.display = 'none'
   renderOverview()
 })()
+
+})();
