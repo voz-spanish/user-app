38c4009e6deb949de1aea3d47898e432e4875543
voz <japones.con.coco@gmail.com>
2026-07-26 07:24:38 +0900

    Update user-home.js

--- user-home/user-home.js
+++ user-home/user-home.js
@@ -1,3 +1,6 @@
+;(function () {
+'use strict'
+
 const SUPABASE_URL = 'https://nsprbshgkxywtwmimkcy.supabase.co'
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5zcHJic2hna3h5d3R3bWlta2N5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgzMzMxMDAsImV4cCI6MjA5MzkwOTEwMH0.g51y4rq3xEDYD9GJoux7UDBeOpXyqYLDptwQ3LHy6b8'
 const { createClient } = supabase
@@ -169,3 +172,5 @@ function initNotifButton() {
     loadNotifBadge(),
   ])
 })()
+
+})();
